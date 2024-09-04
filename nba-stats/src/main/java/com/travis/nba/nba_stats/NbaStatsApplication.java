58aebcb67ee36fb614c15adc0ec4fe6a42429fd5
Travis Altenburger <travis.altenburger@gmail.com>
2024-09-04 16:52:48 -0500

    env

---
+++ nba-stats/src/main/java/com/travis/nba/nba_stats/NbaStatsApplication.java
@@ -0,0 +1,13 @@
+package com.travis.nba.nba_stats;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NbaStatsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NbaStatsApplication.class, args);
+	}
+
+}
